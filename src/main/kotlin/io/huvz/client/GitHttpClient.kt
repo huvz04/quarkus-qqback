55fed80dd58214c622cee001d95f16ba903304b2
huvz <dachen04@outlook.com>
2023-12-24 03:39:21 +0800

    :100:update client&properties

--- src/main/kotlin/io/huvz/client/GitHttpClient.kt
+++ src/main/kotlin/io/huvz/client/GitHttpClient.kt
@@ -1,4 +1,29 @@
 package io.huvz.client
 
+import io.huvz.config.MyAppConfig
+import io.huvz.domain.GiteeApi
+import okhttp3.*
+import java.io.IOException
+
+
 class GitHttpClient {
+
+    val giteekey = MyAppConfig.giteekey;
+    fun get(url: String, name: String): Response? {
+
+        val client = OkHttpClient()
+
+        // 构建请求URL
+        val urlBuilder = HttpUrl.parse(url)!!.newBuilder()
+        urlBuilder.addQueryParameter("p", name)
+        urlBuilder.addQueryParameter("access_token", giteekey)
+        val url1 = urlBuilder.build().toString()
+        val request: Request = Request.Builder()
+            .url(url1)
+            .build()
+
+            // 发送请求并获取响应
+        return client.newCall(request).execute()
+
+    }
 }
